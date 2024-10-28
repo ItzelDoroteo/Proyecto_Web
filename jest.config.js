b719a950125367cbe7a62e8e9ad9182b6c90ab6d
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-28 03:36:28 -0600

    update test

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
 module.exports = {
     // Otras configuraciones de Jest que necesites
     moduleNameMapper: {
-      '\\.(css|less|scss)$': 'identity-obj-proxy',
+      '\\.(css|less)$': '<rootDir>/src/__tests__/styleMock.js',
     },
     transform: {
       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
       '^.+\\.css$': 'jest-transform-stub',
+      '^.+\\.css$': 'jest-css-modules',
     },
     testEnvironment: 'jsdom',
+    transformIgnorePatterns: [
+      '/node_modules/(?!primeicons|primereact).+\\.js$',
+    ],
     
   };
   
